4fd087d2cb0c01d91707b13922198b86bebcf4ea
waqaskhansahb <waqasahmed3766@gmail.com>
2024-11-04 00:23:20 +0500

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
+      {/* <Navbar/> */}
     </div>
   )}
     
